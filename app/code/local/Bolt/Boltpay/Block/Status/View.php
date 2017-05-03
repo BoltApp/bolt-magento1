836ab0552719650334178bf55ccabc4ca27ebf61
Akshaya Srivatsa <aks@bolt.me>
2017-05-02 20:34:51 -0700

    WIP

--- app/code/local/Bolt/Boltpay/Block/Status/View.php
+++ app/code/local/Bolt/Boltpay/Block/Status/View.php
@@ -69,18 +69,6 @@ class Bolt_Boltpay_Block_Status_View extends Mage_Adminhtml_Block_Template {
         $result = $boltApi->transmit('ABCD-1234-EFGH', null, 'merchant', 'transactions');
         $resp = array();
         if ($result != null) {
-            if (strlen($result->description) != 0) {
-                $resp['name'] = $result->description;
-            }
-            if (strlen($result->public_id) != 0) {
-                $resp['public_id'] = $result->public_id;
-            }
-            if (strlen($result->public_id) != 0) {
-                $resp['support_phone'] = $result->support_phone;
-            }
-            if (strlen($result->public_id) != 0) {
-                $resp['support_email'] = $result->support_email;
-            }
             return json_encode($result, JSON_PRETTY_PRINT);
         }
         return "No response from Bolt Backend";
