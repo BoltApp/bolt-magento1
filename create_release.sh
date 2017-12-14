1edf86b794f3b52b508aa41fdbadcf0d88f33796
Kazuki Nishiura <kazuki@bolt.me>
2017-12-14 10:17:28 -0800

    Add script to generate zip for magento plugin

--- create_release.sh
+++ create_release.sh
@@ -6,5 +6,9 @@ mkdir /tmp/bolt_magento_plugin
 cp -r app /tmp/bolt_magento_plugin/.
 cp -r lib /tmp/bolt_magento_plugin/.
 cp -r skin /tmp/bolt_magento_plugin/.
+find /tmp/bolt_magento_plugin -name ".DS_Store" -type f -delete
 
 tar -C /tmp -cvf bolt_magento_plugin.tar.gz bolt_magento_plugin
+cd /tmp/bolt_magento_plugin
+zip -r magento.zip *
+echo /tmp/bolt_magento_plugin/magento.zip
