59b3af1dc0a31496f57a347b9d3847d048aa42df
Kazuki Nishiura <kazuki@bolt.me>
2017-12-19 09:55:45 -0800

    Update magento/create_release script to copy zip to proper path

--- create_release.sh
+++ create_release.sh
@@ -9,6 +9,7 @@ cp -r skin /tmp/bolt_magento_plugin/.
 find /tmp/bolt_magento_plugin -name ".DS_Store" -type f -delete
 
 tar -C /tmp -cvf bolt_magento_plugin.tar.gz bolt_magento_plugin
+current_dir=$(pwd)
 cd /tmp/bolt_magento_plugin
 zip -r magento.zip *
-echo /tmp/bolt_magento_plugin/magento.zip
+cp magento.zip $current_dir/.
