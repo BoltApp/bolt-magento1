ddbf329fdfd15b28c820b08acc1e66613062ae93
daisy1754 <d-part@hotmail.co.jp>
2018-04-15 13:29:00 -0700

    Add shell script to construct zip file

---
+++ create_release.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+if [ $# -le 0 ]; then
+    echo "You must specify version number. \nExample: ./create_release.sh 1.0.8"
+    exit -1
+fi
+
+rm -fr /tmp/bolt_magento_plugin
+
+mkdir /tmp/bolt_magento_plugin
+cp -r app /tmp/bolt_magento_plugin/.
+cp -r lib /tmp/bolt_magento_plugin/.
+cp -r skin /tmp/bolt_magento_plugin/.
+cp CHANGELOG.md /tmp/bolt_magento_plugin/.
+find /tmp/bolt_magento_plugin -name ".DS_Store" -type f -delete
+
+current_dir=$(pwd)
+cd /tmp/bolt_magento_plugin
+zip -r bolt-magento1-v$1.zip *
+cp bolt-magento1-v$1.zip $current_dir/.
+
+echo "\n\nZip file bolt-magento1-v$1.zip created."
