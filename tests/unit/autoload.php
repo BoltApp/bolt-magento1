876e71f6ae0c61ff01f2b150570ef620c761f293
YevhenShBolt <yevhen@bolt.com>
2018-04-27 12:54:13 +0300

    Added phpunit phar file

--- tests/unit/autoload.php
+++ tests/unit/autoload.php
@@ -2,7 +2,7 @@
 
 ini_set(
     'include_path', ini_get('include_path') . PATH_SEPARATOR
-    . dirname(__FILE__) . '/../../app' . PATH_SEPARATOR . dirname(__FILE__)
+    . dirname(__FILE__) . '/../../app' . PATH_SEPARATOR
 );
 ini_set(
     'include_path', ini_get('include_path') . PATH_SEPARATOR
@@ -17,9 +17,12 @@ error_reporting(E_ALL | E_STRICT);
 
 //Set custom memory limit
 ini_set('memory_limit', '512M');
+
 //Include Magento libraries
 require_once 'Mage.php';
+
 //Start the Magento application
 Mage::app('default');
+
 //Avoid issues "Headers already send"
 session_start();
