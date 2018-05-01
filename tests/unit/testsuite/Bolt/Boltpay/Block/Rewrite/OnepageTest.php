7232875446263588ef17862305d4ddb3410068c8
YevhenShBolt <yevhen@bolt.com>
2018-05-01 15:02:42 +0300

    PHPUnitTests: added phpDocComments to the class Bolt_Boltpay_Block_Rewrite_OnepageTest

--- tests/unit/testsuite/Bolt/Boltpay/Block/Rewrite/OnepageTest.php
+++ tests/unit/testsuite/Bolt/Boltpay/Block/Rewrite/OnepageTest.php
@@ -1,8 +1,15 @@
 <?php
 
+/**
+ * Class Bolt_Boltpay_Block_Rewrite_OnepageTest
+ */
 class Bolt_Boltpay_Block_Rewrite_OnepageTest extends PHPUnit_Framework_TestCase
 {
     private $app = null;
+
+    /**
+     * @var string
+     */
     private $onepageRewriteClass = null;
 
     public function setUp() 
@@ -11,43 +18,55 @@ class Bolt_Boltpay_Block_Rewrite_OnepageTest extends PHPUnit_Framework_TestCase
         $this->onepageRewriteClass = Mage::getConfig()->getBlockClassName('checkout/onepage');
     }
 
-    public function testOnepageCheckoutBlockIsOverridenCorrectly() 
+    /**
+     * @inheritdoc
+     */
+    public function testOnepageCheckoutBlockIsOverridenCorrectly()
     {
         $this->assertEquals('Bolt_Boltpay_Block_Rewrite_Onepage', $this->onepageRewriteClass);
     }
 
     /**
-     * @skip - because travis CI have fatal errror
+     * @inheritdoc
      */
     public function testStepsWhenSkipPaymentIsTrue()
     {
         $this->app->getStore()->setConfig('payment/boltpay/skip_payment', 1);
         $onepageRewrite = new $this->onepageRewriteClass;
+
+        $result = array(
+            'login',
+            'billing',
+            'shipping',
+            'shipping_method',
+            'review'
+        );
+
         $this->assertEquals(
-            array(
-                'login',
-                'billing',
-                'shipping',
-                'shipping_method',
-                'review'
-            ),
+            $result,
             array_keys($onepageRewrite->getSteps())
         );
     }
 
-    public function testStepsWhenSkipPaymentIsFalse() 
+    /**
+     * @inheritdoc
+     */
+    public function testStepsWhenSkipPaymentIsFalse()
     {
         $this->app->getStore()->setConfig('payment/boltpay/skip_payment', 0);
         $onepageRewrite = new $this->onepageRewriteClass;
+
+        $result = array(
+            'login',
+            'billing',
+            'shipping',
+            'shipping_method',
+            'payment',
+            'review'
+        );
+
         $this->assertEquals(
-            array(
-                'login',
-                'billing',
-                'shipping',
-                'shipping_method',
-                'payment',
-                'review'
-            ),
+            $result,
             array_keys($onepageRewrite->getSteps())
         );
     }
