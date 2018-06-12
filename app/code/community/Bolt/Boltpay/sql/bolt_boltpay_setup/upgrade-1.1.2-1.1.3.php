94a80b376bf7c75ad4ab64166cf128b29b8228b6
John McCombs <jmccombs@coredevelop.com>
2018-06-12 14:57:12 -0400

    Added index for parent_quote_id on sales_flat_quote table

---
+++ app/code/community/Bolt/Boltpay/sql/bolt_boltpay_setup/upgrade-1.1.2-1.1.3.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Magento
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@magento.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade the Bolt extension
+ * to a newer versions in the future. If you wish to customize this extension
+ * for your needs please refer to http://www.magento.com for more information.
+ *
+ * @category   Bolt
+ * @package    Bolt_Boltpay
+ * @copyright  Copyright (c) 2018 Bolt Financial, Inc (http://www.bolt.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+/**
+ * This installer adds an index for sales_flat_quote->parent_quote_id
+ *
+/** @var Mage_Core_Model_Resource_Setup $installer */
+$installer = $this;
+$installer->startSetup();
+
+$setup = new Mage_Eav_Model_Entity_Setup('core_setup');
+
+Mage::log('Installing Bolt 1.1.3 updates', null, 'bolt_install.log');
+
+$quoteTable = $installer->getTable('sales/quote');
+
+if ($installer->getConnection()->isTableExists($quoteTable)) {
+    $table = $installer->getConnection();
+    $table->addIndex(
+        $quoteTable,
+        $installer->getIdxName(
+            'sales/quote',
+            'parent_quote_id'
+        ),
+        'parent_quote_id'
+    );
+}
+
+Mage::log('Bolt 1.1.3 updates installation completed', null, 'bolt_install.log');
+
+$installer->endSetup();
