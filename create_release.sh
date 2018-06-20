365d502b931df47aa4435c3cef0a056773e752c1
daisy1754 <d-part@hotmail.co.jp>
2018-06-19 22:09:37 -0700

    Tweak release script

--- create_release.sh
+++ create_release.sh
@@ -13,11 +13,14 @@ cp -r app /tmp/bolt_magento_plugin/.
 cp -r lib /tmp/bolt_magento_plugin/.
 cp -r skin /tmp/bolt_magento_plugin/.
 cp CHANGELOG.md /tmp/bolt_magento_plugin/.
+# cp package.xml /tmp/bolt_magento_plugin/.
 find /tmp/bolt_magento_plugin -name ".DS_Store" -type f -delete
 
 current_dir=$(pwd)
 cd /tmp/bolt_magento_plugin
 zip -r bolt-magento1-v$1.zip *
 cp bolt-magento1-v$1.zip $current_dir/.
+# tar -cvzf bolt-magento1-v1-$1.tgz *
+# cp bolt-magento1-v1-$1.tgz $current_dir/.
 
 echo "\n\nZip file bolt-magento1-v$1.zip created."
