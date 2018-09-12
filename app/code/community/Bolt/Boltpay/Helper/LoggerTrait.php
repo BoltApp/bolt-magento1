ae2a4fc984dd03fc5a876e17eae91a435b9ffbdb
dumega <phpdev2012@126.com>
2018-09-11 22:18:03 -0700

    Add generic logger trait and helper

---
+++ app/code/community/Bolt/Boltpay/Helper/LoggerTrait.php
@@ -0,0 +1,147 @@
+<?php
+
+/**
+ * Describes log levels.
+ */
+class Bolt_Boltpay_Helper_LogLevel
+{
+    const EMERGENCY = 'emergency';
+    const ALERT     = 'alert';
+    const CRITICAL  = 'critical';
+    const ERROR     = 'error';
+    const WARNING   = 'warning';
+    const NOTICE    = 'notice';
+    const INFO      = 'info';
+    const DEBUG     = 'debug';
+}
+
+/**
+ * This is a simple Logger trait that classes can include.
+ *
+ * It simply delegates all log-level-specific methods to the `log` method to
+ * reduce boilerplate code that a simple Logger that does the same thing with
+ * messages regardless of the error level has to implement.
+ */
+trait Bolt_Boltpay_Helper_LoggerTrait
+{
+    
+    public static $isLoggerEnabled = true;
+    
+    /**
+     * System is unusable.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function emergency($message, array $context = array())
+    {
+        $this->log(Bolt_Boltpay_Helper_LogLevel::EMERGENCY, $message, $context);
+    }
+    /**
+     * Action must be taken immediately.
+     *
+     * Example: Entire website down, database unavailable, etc. This should
+     * trigger the SMS alerts and wake you up.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function alert($message, array $context = array())
+    {
+        $this->log(Bolt_Boltpay_Helper_LogLevel::ALERT, $message, $context);
+    }
+    /**
+     * Critical conditions.
+     *
+     * Example: Application component unavailable, unexpected exception.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function critical($message, array $context = array())
+    {
+        $this->log(Bolt_Boltpay_Helper_LogLevel::CRITICAL, $message, $context);
+    }
+    /**
+     * Runtime errors that do not require immediate action but should typically
+     * be logged and monitored.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function error($message, array $context = array())
+    {
+        $this->log(Bolt_Boltpay_Helper_LogLevel::ERROR, $message, $context);
+    }
+    /**
+     * Exceptional occurrences that are not errors.
+     *
+     * Example: Use of deprecated APIs, poor use of an API, undesirable things
+     * that are not necessarily wrong.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function warning($message, array $context = array())
+    {
+        $this->log(Bolt_Boltpay_Helper_LogLevel::WARNING, $message, $context);
+    }
+    /**
+     * Normal but significant events.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function notice($message, array $context = array())
+    {
+        $this->log(Bolt_Boltpay_Helper_LogLevel::NOTICE, $message, $context);
+    }
+    /**
+     * Interesting events.
+     *
+     * Example: User logs in, SQL logs.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function info($message, array $context = array())
+    {
+        $this->log(Bolt_Boltpay_Helper_LogLevel::INFO, $message, $context);
+    }
+    /**
+     * Detailed debug information.
+     *
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    public function debug($message, array $context = array())
+    {
+        $this->log(Bolt_Boltpay_Helper_LogLevel::DEBUG, $message, $context);
+    }
+    /**
+     * Logs with an arbitrary level.
+     *
+     * @param mixed  $level
+     * @param string $message
+     * @param array  $context
+     *
+     * @return void
+     */
+    abstract public function log($level, $message, array $context = array());
+}
