45232b0980ceca5323012d31b0f820cb7897d8ae
daisy1754 <d-part@hotmail.co.jp>
2018-12-02 21:27:23 -0800

    Use circleCI instead of travisCI

--- tests/scripts/run_integration_tests.sh
+++ tests/scripts/run_integration_tests.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 
-red=`tput setaf 1`
-green=`tput setaf 2`
-reset=`tput sgr0`
+set -e
+set -u
+set -x
 
-cd /home/travis/build/BoltApp/bolt-magento1/
-
-git clone https://${INTEGRATION_TESTS_TOKEN}@github.com/BoltApp/integration-tests.git/
-
+mkdir -p $SCREENSHOT_DIR
+git clone git@github.com:BoltApp/integration-tests.git
 cd integration-tests
-
 npm install
-
-TEST_ENV=sandbox xvfb-run npm run test-checkout-magento1
+TEST_ENV=sandbox npm run test-checkout-magento1
