a21f93911590ef225d255a0d39b12615d6345a50
mccottry <leonmccottry@gmail.com>
2019-02-11 21:29:20 +0100

    Update ShippingController.php
    code indifferent to type of variable returned by $quote->getAppliedRuleIds()

--- app/code/community/Bolt/Boltpay/controllers/ShippingController.php
+++ app/code/community/Bolt/Boltpay/controllers/ShippingController.php
@@ -312,10 +312,7 @@ class Bolt_Boltpay_ShippingController extends Mage_Core_Controller_Front_Action
         }
 
         // include any discounts or gift card rules because they may affect shipping
-        $rules = explode( ",", $quote->getAppliedRuleIds() );
-        foreach($rules as $ruleId) {
-            $cacheIdentifier .= '_applied-rule-'.$ruleId;
-        }
+        $cacheIdentifier .= '_applied-rules-'.json_encode($quote->getAppliedRuleIds());
 
         return md5($cacheIdentifier);
     }
