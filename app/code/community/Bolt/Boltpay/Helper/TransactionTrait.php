a876434841931f5cda3f07283c17a3122cd416bc
daisy1754 <d-part@hotmail.co.jp>
2019-03-28 19:36:02 -0700

    Fix codesniffer warning around strpos

--- app/code/community/Bolt/Boltpay/Helper/TransactionTrait.php
+++ app/code/community/Bolt/Boltpay/Helper/TransactionTrait.php
@@ -32,7 +32,7 @@ trait Bolt_Boltpay_Helper_TransactionTrait {
      * @return string  The immutable quote id
      */
     public function getImmutableQuoteIdFromTransaction( $transaction ) {
-        if (strpos($transaction->order->cart->display_id, '|')) {
+        if (strpos($transaction->order->cart->display_id, '|') !== false) {
             return explode("|", $transaction->order->cart->display_id)[1];
         } else {
             /////////////////////////////////////////////////////////////////
@@ -69,7 +69,7 @@ trait Bolt_Boltpay_Helper_TransactionTrait {
      * @return string  The order increment id
      */
     public function getIncrementIdFromTransaction( $transaction ) {
-        return (strpos($transaction->order->cart->display_id, '|'))
+        return (strpos($transaction->order->cart->display_id, '|') !== false)
             ? explode("|", $transaction->order->cart->display_id)[0]
             : $transaction->order->cart->display_id;
     }
