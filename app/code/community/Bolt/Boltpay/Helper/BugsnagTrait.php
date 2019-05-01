12955cf04a6fc2aeefc0bbd8fbcda8bbb1b2f928
daisy1754 <d-part@hotmail.co.jp>
2019-05-01 16:59:30 -0700

    indent

--- app/code/community/Bolt/Boltpay/Helper/BugsnagTrait.php
+++ app/code/community/Bolt/Boltpay/Helper/BugsnagTrait.php
@@ -56,7 +56,7 @@ trait Bolt_Boltpay_Helper_BugsnagTrait {
             } else {
                 $bugsnag->setReleaseStage(Mage::getStoreConfig('payment/boltpay/test') ? 'development' : 'production');
             }
-			$bugsnag->setNotifyReleaseStages( array( 'development', 'production' ) );
+            $bugsnag->setNotifyReleaseStages( array( 'development', 'production' ) );
             $bugsnag->setAppVersion(static::getBoltPluginVersion());
             $bugsnag->setBatchSending(true);
             $bugsnag->setBeforeNotifyFunction(array($this, 'beforeNotifyFunction'));
