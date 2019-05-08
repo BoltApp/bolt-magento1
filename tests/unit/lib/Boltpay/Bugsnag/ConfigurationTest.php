e433fc7386f6d801f2f28802d0006ce8ef2d8741
oldDriver <alex.emelyanov.ua@gmail.com>
2019-05-08 16:55:58 +0300

    Unit tests in progress

--- tests/unit/lib/Boltpay/Bugsnag/ConfigurationTest.php
+++ tests/unit/lib/Boltpay/Bugsnag/ConfigurationTest.php
@@ -43,8 +43,180 @@ class ConfigurationTest extends TestCase
         $this->assertInternalType('boolean', $object->batchSending);
         $this->assertInternalType('boolean', $object->useSSL);
         $this->assertInternalType('null', $object->endpoint);
-        
-        
+        $this->assertInternalType('null', $object->notifyReleaseStages);
+        $this->assertInternaltype('array', $object->filters);
+        $this->assertInternalType('null', $object->projectRoot);
+        $this->assertInternalType('array', $object->proxySettings);
+        $this->assertInternalType('array', $object->notifier);
+        $this->assertInternalType('boolean', $object->sendEnvironment);
+        $this->assertInternalType('boolean', $object->sendCookies);
+        $this->assertInternalType('boolean', $object->sendSession);
+        $this->assertInternalType('boolean', $object->sendCode);
+        $this->assertInternalType('null', $object->stripPath);
+        $this->assertInternalType('null', $object->stripPathRegex);
+        $this->assertInternalType('null', $object->context);
+        $this->assertInternalType('null', $object->type);
+        $this->assertInternalType('null', $object->user);
+        $this->assertInternalType('string', $object->releaseStage);
+        $this->assertInternalType('null', $object->appVersion);
+        $this->assertInternalType('null', $object->hostname);
+        $this->assertInternalType('null', $object->metaData);
+        $this->assertInternalType('null', $object->beforeNotifyFunction);
+        $this->assertInternalType('null', $object->errorReportingLevel);
+        $this->assertInternalType('array', $object->curlOptions);
+        $this->assertInternalType('boolean', $object->debug);
+        // Default values
         $this->assertEmpty($object->apiKey);
+        $this->assertTrue($object->autoNotify);
+        $this->assertTrue($object->batchSending);
+        $this->assertTrue($object->useSSL);
+        $this->assertEmpty($object->endpoint);
+        $this->assertEmpty($object->notifyReleaseStages);
+        $this->assertEquals(['password'], $object->filters);
+        $this->assertEmpty($object->projectRoot);
+        $this->assertEmpty($object->projectRootRegex);
+        $this->assertEmpty($object->proxySettings);
+        $this->assertEquals([
+            'name' => 'Bugsnag PHP (Official)',
+            'version' => '2.9.2',
+            'url' => 'https://bugsnag.com',
+        ], $object->notifier);
+        $this->assertFalse($object->sendEnvironment);
+        $this->assertTrue($object->sendCookies);
+        $this->assertTrue($object->sendSession);
+        $this->assertTrue($object->sendCode);
+        $this->assertEmpty($object->stripPath);
+        $this->assertEmpty($object->stripPathRegex);
+        $this->assertEmpty($object->context);
+        $this->assertEmpty($object->type);
+        $this->assertEmpty($object->user);
+        $this->assertEquals('production', $object->releaseStage);
+        $this->assertEmpty($object->appVersion);
+        $this->assertEmpty($object->hostname);
+        $this->assertEmpty($object->metaData);
+        $this->assertEmpty($object->beforeNotifyFunction);
+        $this->assertEmpty($object->errorReportingLevel);
+        $this->assertEmpty($object->curlOptions);
+        $this->assertFalse($object->debug);
+    }
+
+    /**
+     * @test
+     * @dataProvider getNotifyEndpointCases
+     */
+    public function getNotifyEndpoint($data)
+    {
+        $object = new Bugsnag_Configuration();
+        $object->endpoint = $data['endpoint'];
+        $object->useSSL = $data['useSSL'];
+        $this->assertEquals($data['expect'], $object->getNotifyEndpoint());
+    }
+
+    /**
+     * @return array
+     */
+    public function getNotifyEndpointCases()
+    {
+        return [
+            [
+                'data' => [
+                    'endpoint' => null,
+                    'useSSL' => false,
+                    'expect' => 'http://notify.bugsnag.com'
+                ]
+            ],
+            [
+                'data' => [
+                    'endpoint' => null,
+                    'useSSL' => true,
+                    'expect' => 'https://notify.bugsnag.com'
+                ]
+            ],
+            // this is not right behaviour
+            [
+                'data' => [
+                    'endpoint' => '',
+                    'useSSL' => false,
+                    'expect' => 'http://'
+                ]
+            ],
+            [
+                'data' => [
+                    'endpoint' => '',
+                    'useSSL' => true,
+                    'expect' => 'https://'
+                ]
+            ],
+            [
+                'data' => [
+                    'endpoint' => 'bolt.com',
+                    'useSSL' => false,
+                    'expect' => 'http://bolt.com'
+                ]
+            ],
+            [
+                'data' => [
+                    'endpoint' => 'bolt.com',
+                    'useSSL' => true,
+                    'expect' => 'https://bolt.com'
+                ]
+            ],
+            [
+                'data' => [
+                    'endpoint' => 'http://bolt.com',
+                    'useSSL' => false,
+                    'expect' => 'http://bolt.com'
+                ]
+            ],
+            [
+                'data' => [
+                    'endpoint' => 'http://bolt.com',
+                    'useSSL' => true,
+                    'expect' => 'http://bolt.com'
+                ]
+            ],
+            [
+                'data' => [
+                    'endpoint' => 'https://bolt.com',
+                    'useSSL' => false,
+                    'expect' => 'https://bolt.com'
+                ]
+            ],
+            [
+                'data' => [
+                    'endpoint' => 'https://bolt.com',
+                    'useSSL' => true,
+                    'expect' => 'https://bolt.com'
+                ]
+            ],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider shouldNotifyCases
+     */
+    public function shouldNotify($data)
+    {
+        $object = new Bugsnag_Configuration();
+        $object->notifyReleaseStages = $data['notifyReleaseStages'];
+        $object->releaseStage = $data['releaseStage'];
+        $this->assertEquals($data['expect'], $object->shouldNotify());
+    }
+
+    /**
+     * @return array
+     */
+    public function shouldNotifyCases()
+    {
+        return [
+            [
+                'data' => [
+                    'notifyReleaseStages' => [],
+                    'releaseStage' => '',
+                    'expect' => ''
+                ]
+            ]
+        ];
     }
 }
