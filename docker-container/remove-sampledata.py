8a0de9cea2deb296924a1bb9e9c4708fea78cc82
ethanwayda22 <ewayda2222@gmail.com>
2019-06-16 19:32:42 -0700

    removed file that is not used

--- docker-container/remove-sampledata.py
+++
@@ -1,8 +0,0 @@
-# Removes line that installs sample data
-
-with open("docker-magento/Dockerfile", "r") as f:
-    lines = f.readlines()
-with open("docker-magento/Dockerfile", "w") as f:
-    for line in lines:
-        if line.strip("\n") != "RUN chmod +x /usr/local/bin/install-sampledata":
-            f.write(line)
