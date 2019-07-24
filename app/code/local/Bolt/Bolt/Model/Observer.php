a6f8f5a2e3f5427b8caf0d64148a9738c2f94980
Leon McCottry <leon@guaranteed.network>
2019-07-24 14:47:46 +0200

    merge

--- app/code/local/Bolt/Bolt/Model/Observer.php
+++ app/code/local/Bolt/Bolt/Model/Observer.php
@@ -2,6 +2,8 @@
 
 class Bolt_Bolt_Model_Observer extends Amasty_Rules_Model_Observer
 {
+    use Bolt_Boltpay_BoltGlobalTrait;
+
     /**
      * @param $observer
      * Process quote item validation and discount calculation
@@ -12,6 +14,76 @@ class Bolt_Bolt_Model_Observer extends Amasty_Rules_Model_Observer
         $promotions =  Mage::getModel('amrules/promotions');
         $promotions->process($observer);
         return $this;
+    }
+
+    /**
+     * Skips all In-Store pickup shipping options
+     *
+     * event: bolt_boltpay_shipping_method_applied_after
+     *
+     * @param $observer
+     */
+    function skipInStorePickup($observer) {
+        $quote = $observer->getQuote();
+        $shippingMethodCode = $observer->getShippingMethodCode();
+
+        /** @var Mage_Sales_Model_Quote_Address_Rate $shippingRate */
+        $shippingRate = Mage::getModel('sales/quote_address_rate')->load($shippingMethodCode, 'code');
+
+        if (strpos($shippingRate->getMethodTitle(), 'In-Store Pick Up') !== false) {
+            $quote->setShouldSkipThisShippingMethod(true);
+        }
+    }
+
+    /**
+     * Add time estimation to shipping labels
+     *
+     * event: bolt_boltpay_filter_shipping_label
+     *
+     * @param $observer
+     */
+    public function addShippingTimeToLabel($observer) {
+        $valueWrapper = $observer->getValueWrapper();
+        $shippingLabel = $valueWrapper->getValue();
+
+        /** @var Mage_Sales_Model_Quote_Address_Rate $shippingRate */
+        $shippingRate = $observer->getParameters();
+
+        $carrier = $shippingRate->getCarrierTitle();
+        $title = $shippingRate->getMethodTitle();
+
+        // Apply adhoc rules to return concise string.
+        if ($carrier === "Shipping Table Rates") {
+            $methodId = explode('amtable_amtable', $shippingRate->getCode());
+            $methodId = isset($methodId[1]) ? $methodId[1] : false;
+            $estimateDelivery = $methodId ? Mage::helper('arrivaldates')->getEstimateHtml($methodId, false) : '';
+            $shippingLabel =  $estimateDelivery ? $title . ' -- ' . $estimateDelivery : $title;
+        }
+
+        $valueWrapper->setValue($shippingLabel);
+    }
+
+    /**
+     * Sets in-store pickup to the session
+     *
+     * event: bolt_boltpay_order_creation_before
+     * @param $observer
+     */
+    public function setInStorePickup($observer) {
+
+        /** @var Mage_Sales_Model_Order $order */
+        $order = $observer->getOrder();
+        /** @var Mage_Sales_Model_Quote $immutableQuote */
+        $immutableQuote = $observer->getImmutableQuote();
+        $transaction = $observer->getTransaction();
+
+        $packagesToShip = @$transaction->order->cart->shipments;
+
+        if (!$packagesToShip && !$immutableQuote->isVirtual()) {
+            if ($immutableQuote->getStorePickupId()) {
+                Mage::getSingleton('core/session')->setIsStorePickup(true);
+            }
+        }
     }
 
     /**
@@ -38,7 +110,7 @@ class Bolt_Bolt_Model_Observer extends Amasty_Rules_Model_Observer
     /**
      * Adds the user note to the Bolt order data if it has already been set in the session
      *
-     * event: bolt_boltpay_filter_boltOrder
+     * event: bolt_boltpay_filter_bolt_order
      *
      * @param $observer
      */
@@ -53,5 +125,46 @@ class Bolt_Bolt_Model_Observer extends Amasty_Rules_Model_Observer
         }
 
         $valueWrapper->setValue($orderData);
+    }
+
+    /**
+     * Adds the user note to the Bolt order data if it has already been set in the session
+     *
+     * event: bolt_boltpay_filter_discount_amount
+     *
+     * @param $observer
+     */
+    public function adjustAmastyDiscount($observer) {
+
+        $valueWrapper = $observer->getValueWrapper();
+        $parameters = $observer->getParameters();
+
+        $old = $discountAmount = $valueWrapper->getValue();
+        $quote = $parameters['quote'];
+        $discountType = $parameters['discount'];
+
+        if($discountType=='amgiftcard'){
+            $discountAmount = $quote->getAmGiftCardsAmount();
+$this->boltHelper()->notifyException(new Exception("Old Amstay Gift Card Discount: $old, New: $discountAmount"));
+        }elseif ($discountType == 'amstcred') {
+            $customerId = $quote->getCustomer()->getId();
+            if ($customerId) $discountAmount = $this->getStoreCreditBalance($customerId);
+$this->boltHelper()->notifyException(new Exception("Old Amstay Credit Discount: $old, New: $discountAmount"));
+        }
+
+        $valueWrapper->setValue($discountAmount);
+    }
+
+    /**
+     * Function get store credit balance of customer
+     *
+     * @param $customerId
+     *
+     * @return mixed
+     */
+    private function getStoreCreditBalance($customerId)
+    {
+        return Mage::getModel('amstcred/balance')->getCollection()
+            ->addFilter('customer_id', $customerId)->getFirstitem()->getAmount();
     }
 }
