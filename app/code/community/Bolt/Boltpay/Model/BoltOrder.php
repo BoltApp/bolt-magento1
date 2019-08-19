0b97b09071c5761185cc327b79bd48356b05c9c5
John McCombs <john.mccombs@bolt.com>
2019-08-19 21:59:20 +0700

    Update billing address email with shipping address email if it's empty

--- app/code/community/Bolt/Boltpay/Model/BoltOrder.php
+++ app/code/community/Bolt/Boltpay/Model/BoltOrder.php
@@ -593,6 +593,10 @@ class Bolt_Boltpay_Model_BoltOrder extends Bolt_Boltpay_Model_Abstract
             $billingAddress->setCompany($fallbackAddress->getCompany());
         }
 
+        if (!trim($billingAddress->getEmail())) {
+            $billingAddress->setEmail($fallbackAddress->getEmail());
+        }
+
         if ($wasCorrected) {
             $billingAddress->save();
             $quote->save();
