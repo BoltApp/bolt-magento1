84b5fedc2399beceef87cd25225a61e9f0099868
Aden Dong <aden@bolt.com>
2019-08-22 13:33:47 -0400

    Missed a file it seems

--- docker-container/set-magento.php
+++ docker-container/set-magento.php
@@ -21,9 +21,9 @@ Mage::getModel('core/config')->saveConfig('currency/options/base', "USD");
 Mage::getModel('core/config')->saveConfig('currency/options/default', "USD");
 Mage::getModel('core/config')->saveConfig('payment/boltpay/active', 1);
 Mage::getModel('core/config')->saveConfig('payment/boltpay/test', 1);
-Mage::getModel('core/config')->saveConfig('payment/boltpay/api_key', $api_key);
-Mage::getModel('core/config')->saveConfig('payment/boltpay/signing_key', $signing_key);
-Mage::getModel('core/config')->saveConfig('payment/boltpay/publishable_key_multipage', $publishable_key_multipage);
+Mage::getModel('core/config')->saveConfig('payment/keys/api_key', $api_key);
+Mage::getModel('core/config')->saveConfig('payment/keys/signing_key', $signing_key);
+Mage::getModel('core/config')->saveConfig('payment/keys/publishable_key_multipage', $publishable_key_multipage);
 
 # Flushes cache so that changes take place
 Mage::app()->getCacheInstance()->flush(); 
