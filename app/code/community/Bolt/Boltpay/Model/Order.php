d61bdb45a9b61c9d5a036aafa67ae5bf6de4e492
Leon McCottry <leon@guaranteed.network>
2019-09-13 19:47:31 +0200

    Subtle fixes to validation process

--- app/code/community/Bolt/Boltpay/Model/Order.php
+++ app/code/community/Bolt/Boltpay/Model/Order.php
@@ -350,7 +350,7 @@ class Bolt_Boltpay_Model_Order extends Bolt_Boltpay_Model_Abstract
             );
         }
 
-        if (!$parentQuote->getItemsCount()) {
+        if (!$parentQuote->getItemsCount() && !$immutableQuote->getItemsCount()) {
             throw new Bolt_Boltpay_OrderCreationException(
                 OCE::E_BOLT_CART_HAS_EXPIRED,
                 OCE::E_BOLT_CART_HAS_EXPIRED_TMPL_EMPTY
@@ -579,8 +579,11 @@ class Bolt_Boltpay_Model_Order extends Bolt_Boltpay_Model_Abstract
                 $couponExists = (bool) $magentoCoupon->getId();
 
                 if ($couponExists) {
-                    $magentoCouponCodes = $immutableQuote->getCouponCode() ? explode(',', (string) $immutableQuote->getCouponCode()) : array();
-                    if (!in_array($boltCoupon->reference, $magentoCouponCodes)) {
+                    $magentoCouponCodes = $immutableQuote->getCouponCode()
+                        ? explode(',', trim(strtolower((string) $immutableQuote->getCouponCode())))
+                        : array();
+
+                    if (!in_array(trim(strtolower($boltCoupon->reference)), $magentoCouponCodes)) {
                         /** @var Mage_SalesRule_Model_Rule $rule */
                         $rule = (@$cachedRules[$magentoCoupon->getRuleId()]) ?: Mage::getModel('salesrule/rule')->load($magentoCoupon->getRuleId());
                         $toTime = $rule->getToDate() ? ((int) strtotime($rule->getToDate()) + Mage_CatalogRule_Model_Resource_Rule::SECONDS_IN_DAY - 1) : 0;
