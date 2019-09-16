a2757ef3cce4c82b37dfadc960052df859543b0e
John McCombs <john.mccombs@bolt.com>
2019-09-16 10:16:18 +0700

    Reactivate used promotion after deleting failed order due to auth failure

--- app/code/community/Bolt/Boltpay/Model/Order.php
+++ app/code/community/Bolt/Boltpay/Model/Order.php
@@ -837,7 +837,7 @@ class Bolt_Boltpay_Model_Order extends Bolt_Boltpay_Model_Abstract
 
         $session->unsBoltUserId();
     }
-    
+
     /**
      * Sends an email if an order email has not already been sent.
      *
@@ -941,6 +941,8 @@ class Bolt_Boltpay_Model_Order extends Bolt_Boltpay_Model_Abstract
             $previousStoreId = Mage::app()->getStore()->getId();
             Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
             $order->delete();
+            Mage::dispatchEvent('bolt_boltpay_failed_order_removed_after', array('order' => $order));
+            $this->reactivateUsedPromotion($order);
             Mage::app()->setCurrentStore($previousStoreId);
             ###########################################################
 
@@ -949,6 +951,37 @@ class Bolt_Boltpay_Model_Order extends Bolt_Boltpay_Model_Abstract
             ##########################################################
             $parentQuote->setIsActive(true)->save();
             ###########################################################
+        }
+    }
+
+    /**
+     * Reactivates the promotion if it was used in failed orders
+     *
+     * @param Mage_Sales_Model_Order $order
+     */
+    public function reactivateUsedPromotion(Mage_Sales_Model_Order $order)
+    {
+        try {
+            /** @var Bolt_Boltpay_Model_Coupon $couponModel */
+            $couponModel = Mage::getModel('boltpay/coupon');
+            $customerId = $order->getCustomerId();
+
+            if ($code = $order->getCouponCode()){
+                $coupon = $couponModel->decreaseCouponTimesUsed($code);
+                if ($customerId){
+                    $couponModel->decreaseCustomerCouponTimesUsed($customerId, $coupon);
+                }
+            }
+
+            $ruleIds = explode(',', $order->getAppliedRuleIds());
+            if ($ruleIds && $customerId){
+                foreach ($ruleIds as $ruleId) {
+                    $couponModel->decreaseCustomerRuleTimesUsed($customerId, $ruleId);
+                }
+            }
+        } catch (\Exception $e) {
+            $this->boltHelper()->logException($e);
+            $this->boltHelper()->notifyException($e);
         }
     }
 
