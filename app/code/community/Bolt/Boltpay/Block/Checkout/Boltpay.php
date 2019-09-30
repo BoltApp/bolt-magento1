a43aa1eb3cd5c93049bad617e0c582f2d684cf19
Alex Yemelyanov <alex.emelyanov.ua@gmail.com>
2019-09-30 17:23:39 +0300

    Update Boltpay.php

--- app/code/community/Bolt/Boltpay/Block/Checkout/Boltpay.php
+++ app/code/community/Bolt/Boltpay/Block/Checkout/Boltpay.php
@@ -459,6 +459,15 @@ class Bolt_Boltpay_Block_Checkout_Boltpay extends Mage_Checkout_Block_Onepage_Re
     }
 
     /**
+     * Return disabled customer groups for Bolt
+     * @return array
+     */
+    public function disableCustomerGroups()
+    {
+        return explode(',', Mage::getStoreConfig('payment/boltpay/bolt_disabled_customer_groups'));
+    }
+
+    /**
      * Returns whether enable merchant scoped account.
      * @return string
      */
@@ -613,6 +622,13 @@ class Bolt_Boltpay_Block_Checkout_Boltpay extends Mage_Checkout_Block_Onepage_Re
      */
     private function isAllowedOnCurrentPageByRoute()
     {
+        $isAllowed = false;
+        $quote = $this->getQuote();
+        $customerGroupId = $quote->getCustomerGroupId();
+        $disabledGroups = $this->disableCustomerGroups();
+        if ($customerGroupId && in_array($customerGroupId, $disabledGroups)) {
+            return $isAllowed;
+        }
         $routeName = $this->getRequest()->getRouteName();
         $controllerName = $this->getRequest()->getControllerName();
 
