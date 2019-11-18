2349a7926dbf55f9f1332df34af3cd0e2b04bc29
kajan-bolt <54901047+kajan-bolt@users.noreply.github.com>
2019-11-18 09:43:20 -0500

    Loadtests for the M1 plugin (#545)
    * Setup an endpoint that lets you create a cart for loadtesting
    
    * fix typo in setup.sh
    
    * Create loadtests for M1
    
    * remove commented out code
    
    * Add comments to config.js file
    
    * Change underscore variable names to camelCase

---
+++ tests/load/inputs/address.js
@@ -0,0 +1,17 @@
+export const address = {
+    "street_address1": "1210 South Bascom Avenue",
+    "street_address2": "",
+    "street_address3": "",
+    "street_address4": "",
+    "locality": "San Jose",
+    "region": "California",
+    "postal_code": "95128",
+    "company": "",
+    "country": "United States",
+    "country_code": "US",
+    "name": "dummy user",
+    "first_name": "dummy",
+    "last_name": "user",
+    "phone": "4161111111",
+    "email_address": "kajan@bolt.com"
+};
