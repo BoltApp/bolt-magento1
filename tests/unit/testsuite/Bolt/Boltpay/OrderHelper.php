70a1e6c87899656dab509d1a7b2f62b34b65cca8
oldDriver <alex.emelyanov.ua@gmail.com>
2019-11-29 17:09:11 +0200

    Little changes

--- tests/unit/testsuite/Bolt/Boltpay/OrderHelper.php
+++ tests/unit/testsuite/Bolt/Boltpay/OrderHelper.php
@@ -87,7 +87,7 @@ class Bolt_Boltpay_OrderHelper
      * @return Mage_Sales_Model_Order
      * @throws Mage_Core_Exception
      */
-    public static function createDummyOrder($productId, $qty = 2, $paymentMethod = 'checkmo')
+    public static function createDummyOrder($productId, $qty = 2, $paymentMethod = 'boltpay')
     {
         $testHelper = new Bolt_Boltpay_TestHelper();
         $testHelper->addTestBillingAddress();
@@ -145,7 +145,9 @@ class Bolt_Boltpay_OrderHelper
      */
     public static function deleteDummyOrder(Mage_Sales_Model_Order $order)
     {
-        self::deleteDummyOrderByIncrementId($order->getIncrementId());
+        Mage::register('isSecureArea', true);
+        $order->delete();
+        Mage::unregister('isSecureArea');
 
     }
 }
