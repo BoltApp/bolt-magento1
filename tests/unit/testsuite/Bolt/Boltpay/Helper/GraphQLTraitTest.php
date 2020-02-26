a4d392959bddf025b9136eeaa4197037686ba342
Vitaliy Reznikov <vitaliy@bolt.com>
2020-02-26 09:06:51 +0200

    Fix tests for circleCI&php5.5

--- tests/unit/testsuite/Bolt/Boltpay/Helper/GraphQLTraitTest.php
+++ tests/unit/testsuite/Bolt/Boltpay/Helper/GraphQLTraitTest.php
@@ -28,7 +28,7 @@ class Bolt_Boltpay_Helper_GraphQLTest extends PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->disableOriginalClone()
             ->disableArgumentCloning()
-            ->setMethods(array('getContextInfo','addMetaData','getBoltPluginVersion','getApiUrl','notifyException','logException'))
+            ->setMethods(array('getContextInfo', 'addMetaData', 'getBoltPluginVersion', 'getApiUrl', 'notifyException', 'logException'))
             ->getMockForTrait();
         $this->currentMock->method('getBoltPluginVersion')->willReturn('2.3.0');
         $this->currentMock->method('getApiUrl')->willReturn('https://api.bolt.com/');
@@ -56,11 +56,6 @@ class Bolt_Boltpay_Helper_GraphQLTest extends PHPUnit_Framework_TestCase
         );
     }
 
-    private function getFeatureSwitches_test_payload()
-    {
-        return '{"operationName":"GetFeatureSwitches","variables":{"type":"MAGENTO_1","version":"2.3.0"},"query":"query GetFeatureSwitches($type: PluginType!, $version: String!) {\r\n  plugin(type: $type, version: $version) {\r\n    features {\r\n      name\r\n      value\r\n      defaultValue\r\n      rolloutPercentage\r\n    }\r\n  }\r\n}"}';
-    }
-
     /**
      * @test
      */
@@ -68,7 +63,10 @@ class Bolt_Boltpay_Helper_GraphQLTest extends PHPUnit_Framework_TestCase
     {
         $this->guzzleClientMock->expects($this->once())->method('post')->with(
             'https://api.bolt.com/v2/merchant/api',
-            $this->getFeatureSwitches_test_payload(),
+            $this->callback(function ($jsonData) {
+                $testJsonData = '{"operationName":"GetFeatureSwitches","variables":{"type":"MAGENTO_1","version":"2.3.0"},"query":"query GetFeatureSwitches($type: PluginType!, $version: String!) {\n  plugin(type: $type, version: $version) {\n    features {\n      name\n      value\n      defaultValue\n      rolloutPercentage\n    }\n  }\n}"}';
+                return str_replace('\r\n', '\n', $jsonData) == $testJsonData;
+            }),
             $this->anything()
         )->willReturn($this->responseMock);
 
@@ -90,11 +88,7 @@ class Bolt_Boltpay_Helper_GraphQLTest extends PHPUnit_Framework_TestCase
     public function getFeatureSwitches_fail()
     {
         $exception = new Exception('Test exception');
-        $this->guzzleClientMock->expects($this->once())->method('post')->with(
-            'https://api.bolt.com/v2/merchant/api',
-            $this->getFeatureSwitches_test_payload(),
-            $this->anything()
-        )->will($this->throwException($exception));
+        $this->guzzleClientMock->expects($this->once())->method('post')->will($this->throwException($exception));
 
         $this->currentMock->expects($this->once())->method('notifyException')->with($exception);
         $this->currentMock->expects($this->once())->method('logException')->with($exception);
