d74fde1b1fbce6531220af9b8d9604745001cff1
Aden Dong <aden@bolt.com>
2020-03-02 16:40:52 -0500

    done

--- tests/unit/testsuite/Bolt/Boltpay/Helper/ConfigTraitTest.php
+++ tests/unit/testsuite/Bolt/Boltpay/Helper/ConfigTraitTest.php
@@ -14,23 +14,10 @@ class Bolt_Boltpay_Helper_ConfigTraitTest extends PHPUnit_Framework_TestCase
      */
     private $currentMock;
 
-    private $app;
-
     public function setUp()
     {
-        $this->app = Mage::app('default');
-        /** @var Mage_Core_Model_Store $appStore */
-        $appStore = $this->app->getStore();
-        $appStore->resetConfig();
-
-        $this->currentMock = $this->getMockBuilder('Bolt_Boltpay_Helper_ConfigTrait')
-            ->disableOriginalConstructor()
-            ->disableOriginalClone()
-            ->disableArgumentCloning()
-            ->setMethods(['getPaymentBoltpayConfig'])
-            ->getMockForTrait();
-
-        $appStore->setConfig('payment/boltpay/active', 1);
+        Mage::app('default')->getStore()->resetConfig();
+        $this->currentMock = $this->getMockBuilder('Bolt_Boltpay_Helper_ConfigTrait')->getMockForTrait();
     }
 
     /**
@@ -41,12 +28,9 @@ class Bolt_Boltpay_Helper_ConfigTraitTest extends PHPUnit_Framework_TestCase
      */
     public function getAllowedButtonByCustomRoutes_returnsEmptyString_forEmptyConfig()
     {
-        $this->currentMock->expects($this->once())
-            ->method('getPaymentBoltpayConfig')
-            ->with('allowed_button_by_custom_routes', Bolt_Boltpay_Block_Checkout_Boltpay::CHECKOUT_TYPE_MULTI_PAGE)
-            ->will($this->returnValue(''));
-
+        TestHelper::stubConfigValue('payment/boltpay/allowed_button_by_custom_routes', '');
         $this->assertEmpty($this->currentMock->getAllowedButtonByCustomRoutes());
+        TestHelper::restoreOriginals();
     }
 
     /**
@@ -57,14 +41,13 @@ class Bolt_Boltpay_Helper_ConfigTraitTest extends PHPUnit_Framework_TestCase
      */
     public function getAllowedButtonByCustomRoutes_returnsCorrectResult_forCommaSeparatedValues()
     {
-        $this->currentMock->expects($this->once())
-            ->method('getPaymentBoltpayConfig')
-            ->with('allowed_button_by_custom_routes', Bolt_Boltpay_Block_Checkout_Boltpay::CHECKOUT_TYPE_MULTI_PAGE)
-            ->will($this->returnValue("testroute, test-route, \r, \n, \r\n, 0, a"));
-
+        TestHelper::stubConfigValue(
+            'payment/boltpay/allowed_button_by_custom_routes',
+            "testroute, test-route, \r, \n, \r\n, 0, a"
+        );
         $result = ['testroute', 'test-route', '0', 'a'];
-
         $this->assertEquals($result, $this->currentMock->getAllowedButtonByCustomRoutes());
+        TestHelper::restoreOriginals();
     }
 
     /**
@@ -75,14 +58,10 @@ class Bolt_Boltpay_Helper_ConfigTraitTest extends PHPUnit_Framework_TestCase
      */
     public function getAllowedButtonByCustomRoutes_doesNotSplitBySpace_forValuesWithoutComma()
     {
-        $this->currentMock->expects($this->once())
-            ->method('getPaymentBoltpayConfig')
-            ->with('allowed_button_by_custom_routes', Bolt_Boltpay_Block_Checkout_Boltpay::CHECKOUT_TYPE_MULTI_PAGE)
-            ->will($this->returnValue('testroute test-route'));
-
+        TestHelper::stubConfigValue('payment/boltpay/allowed_button_by_custom_routes', 'testroute test-route');
         $result = ['testroute test-route'];
-
         $this->assertEquals($result, $this->currentMock->getAllowedButtonByCustomRoutes());
+        TestHelper::restoreOriginals();
     }
 
     /**
@@ -151,9 +130,9 @@ class Bolt_Boltpay_Helper_ConfigTraitTest extends PHPUnit_Framework_TestCase
      *
      * @dataProvider canUseEverywhere_withVariousConfigs_returnsCorrectResultProvider
      *
-     * @param $active isBoltPayActive
-     * @param $isEverywhere shouldAddButtonEverywhere
-     * @param $expected expected result
+     * @param $active
+     * @param $isEverywhere
+     * @param $expected
      *
      * @throws Mage_Core_Exception
      * @throws Mage_Core_Model_Store_Exception
@@ -169,8 +148,6 @@ class Bolt_Boltpay_Helper_ConfigTraitTest extends PHPUnit_Framework_TestCase
 
     /**
      * Data provider for {@see canUseEverywhere_withVariousConfigs_returnsCorrectResult}
-     *
-     * @return array[]
      */
     public function canUseEverywhere_withVariousConfigs_returnsCorrectResultProvider()
     {
@@ -255,6 +232,94 @@ class Bolt_Boltpay_Helper_ConfigTraitTest extends PHPUnit_Framework_TestCase
         $this->assertTrue($this->currentMock->canUseForCountry('canada'));
         TestHelper::stubConfigValue('payment/boltpay/specificcountry', 'mexico,australia');
         $this->assertFalse($this->currentMock->canUseForCountry('canada'));
+        TestHelper::restoreOriginals();
+    }
+
+    /**
+     * @test
+     * getPaymentBoltpayConfig returns the correct result for various configs
+     *
+     * @covers       Bolt_Boltpay_Helper_ConfigTrait::getPaymentBoltpayConfig
+     *
+     * @dataProvider getPaymentBoltpayConfig_withVariousParams_returnsCorrectResultProvider
+     *
+     * @param $checkoutType
+     * @param $useJsInAdmin
+     * @param $expected
+     *
+     * @throws Mage_Core_Exception
+     * @throws Mage_Core_Model_Store_Exception
+     * @throws ReflectionException
+     */
+    public function getPaymentBoltpayConfig_withVariousParams_returnsCorrectResult($checkoutType, $useJsInAdmin, $expected)
+    {
+        TestHelper::stubConfigValue('payment/boltpay/use_javascript_in_admin', $useJsInAdmin);
+        TestHelper::stubConfigValue('payment/boltpay/specificcountry', 'canada,usa,china');
+        $this->assertEquals($expected, $this->currentMock->getPaymentBoltpayConfig('specificcountry', $checkoutType));
+        TestHelper::restoreOriginals();
+    }
+
+    /**
+     * Data provider for {@see getPaymentBoltpayConfig_withVariousParams_returnsCorrectResult}
+     */
+    public function getPaymentBoltpayConfig_withVariousParams_returnsCorrectResultProvider()
+    {
+        return array(
+            array(Bolt_Boltpay_Block_Checkout_Boltpay::CHECKOUT_TYPE_ADMIN, true, 'canada,usa,china'),
+            array(Bolt_Boltpay_Block_Checkout_Boltpay::CHECKOUT_TYPE_ADMIN, false, ''),
+            array(Bolt_Boltpay_Block_Checkout_Boltpay::CHECKOUT_TYPE_MULTI_PAGE, true, 'canada,usa,china'),
+            array(Bolt_Boltpay_Block_Checkout_Boltpay::CHECKOUT_TYPE_MULTI_PAGE, false, 'canada,usa,china')
+        );
+    }
+
+    /**
+     * @test
+     * various one line methods return the correct config values
+     *
+     * @covers Bolt_Boltpay_Helper_ConfigTrait::isBoltPayActive
+     * @covers Bolt_Boltpay_Helper_ConfigTrait::getPublishableKeyMultiPage
+     * @covers Bolt_Boltpay_Helper_ConfigTrait::getPublishableKeyOnePage
+     * @covers Bolt_Boltpay_Helper_ConfigTrait::getPublishableKeyBackOffice
+     * @covers Bolt_Boltpay_Helper_ConfigTrait::shouldAddButtonEverywhere
+     * @covers Bolt_Boltpay_Helper_ConfigTrait::getBoltPrimaryColor
+     * @covers Bolt_Boltpay_Helper_ConfigTrait::getApiKeyConfig
+     * @covers Bolt_Boltpay_Helper_ConfigTrait::getAdditionalButtonClasses
+     * @covers Bolt_Boltpay_Helper_ConfigTrait::isEnabledProductPageCheckout
+     * @covers Bolt_Boltpay_Helper_ConfigTrait::getProductPageCheckoutSelector
+     * @covers Bolt_Boltpay_Helper_ConfigTrait::getAutoCreateInvoiceAfterCreatingShipment
+     *
+     * @throws Mage_Core_Exception
+     * @throws Mage_Core_Model_Store_Exception
+     * @throws ReflectionException
+     */
+    public function simpleConfigMethods_returnsCorrectConfigValue()
+    {
+        TestHelper::stubConfigValue('payment/boltpay/active', 'false');
+        TestHelper::stubConfigValue('payment/boltpay/publishable_key_multipage', 'multipage key');
+        TestHelper::stubConfigValue('payment/boltpay/publishable_key_onepage', 'onepage key');
+        TestHelper::stubConfigValue('payment/boltpay/publishable_key_admin', 'backoffice key');
+        TestHelper::stubConfigValue('payment/boltpay/add_button_everywhere', 'true');
+        TestHelper::stubConfigValue('payment/boltpay/extra_options', json_encode(array(
+            'boltPrimaryColor' => '#000000',
+            'datadogKey' => 'datadog key'
+        )));
+        TestHelper::stubConfigValue('payment/boltpay/button_classes', 'btnClass');
+        TestHelper::stubConfigValue('payment/boltpay/enable_product_page_checkout', 'true');
+        TestHelper::stubConfigValue('payment/boltpay/product_page_checkout_selector', '.ppcBtn');
+        TestHelper::stubConfigValue('payment/boltpay/auto_create_invoice_after_creating_shipment', true);
+
+        $this->assertFalse($this->currentMock->isBoltPayActive());
+        $this->assertEquals('multipage key', $this->currentMock->getPublishableKeyMultiPage());
+        $this->assertEquals('onepage key', $this->currentMock->getPublishableKeyOnePage());
+        $this->assertEquals('backoffice key', $this->currentMock->getPublishableKeyBackOffice());
+        $this->assertTrue($this->currentMock->shouldAddButtonEverywhere());
+        $this->assertEquals('#000000', $this->currentMock->getBoltPrimaryColor());
+        $this->assertEquals('datadog key', $this->currentMock->getApiKeyConfig());
+        $this->assertEquals('btnClass', $this->currentMock->getAdditionalButtonClasses());
+        $this->assertTrue($this->currentMock->isEnabledProductPageCheckout());
+        $this->assertEquals('.ppcBtn', $this->currentMock->getProductPageCheckoutSelector());
+        $this->assertTrue($this->currentMock->getAutoCreateInvoiceAfterCreatingShipment());
+
         TestHelper::restoreOriginals();
     }
 }
