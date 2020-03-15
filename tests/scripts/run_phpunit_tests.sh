a40ea5845e0bf5bd763594ce234dd5ddd2805783
Aden <adentong@hotmail.com>
2020-03-15 11:50:42 -0400

    [M1P-20] Make tests run faster by disabling coverage for 5.5 (#714)
    * Try to speed up tests
    
    * .
    
    Co-authored-by: Aden Dong <aden@bolt.com>

--- tests/scripts/run_phpunit_tests.sh
+++ tests/scripts/run_phpunit_tests.sh
@@ -7,5 +7,9 @@ set -x
 export TEST_ENV=local
 export PHPUNIT_ENVIRONMENT=true
 
-php ${PHPUNIT_PHAR} --verbose --stderr --report-useless-tests -c tests/unit/phpunit.xml --coverage-clover=./artifacts/coverage.xml
-bash <(curl -s https://bolt-devops.s3-us-west-2.amazonaws.com/testing/codecov_uploader) -f ./artifacts/coverage.xml -F $TEST_ENV
+if [ "$1" == "nocov" ]; then
+  php ${PHPUNIT_PHAR} --verbose --stderr --report-useless-tests -c tests/unit/phpunit.xml
+else
+  php ${PHPUNIT_PHAR} --verbose --stderr --report-useless-tests -c tests/unit/phpunit.xml --coverage-clover=./artifacts/coverage.xml
+  bash <(curl -s https://bolt-devops.s3-us-west-2.amazonaws.com/testing/codecov_uploader) -f ./artifacts/coverage.xml -F $TEST_ENV
+fi
