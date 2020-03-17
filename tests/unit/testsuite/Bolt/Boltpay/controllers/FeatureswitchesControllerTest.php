9e640aefee3b4b1aea986ef68f6cde73edaf0372
Vitaliy Reznikov <vitaliy@bolt.com>
2020-03-17 20:43:17 +0200

    Create webhook which if hit will fetch the latest feature switches from Bolt (#711)
    * Create webhook which if hit will fetch the latest feature switches from Bolt
    
    * Remove commented line
    
    * Changes after review

---
+++ tests/unit/testsuite/Bolt/Boltpay/controllers/FeatureswitchesControllerTest.php
@@ -0,0 +1,98 @@
+<?php
+
+require_once 'Bolt/Boltpay/controllers/FeatureswitchesController.php';
+require_once 'MockingTrait.php';
+require_once 'TestHelper.php';
+
+use Bolt_Boltpay_TestHelper as TestHelper;
+
+/**
+ * @coversDefaultClass Bolt_Boltpay_FeatureswitchesController
+ */
+class Bolt_Boltpay_FeatureswitchesControllerTest extends PHPUnit_Framework_TestCase
+{
+    use Bolt_Boltpay_MockingTrait;
+
+    /** @var string Name of the class tested */
+    protected $testClassName = 'Bolt_Boltpay_FeatureswitchesController';
+
+    /**
+     * @var PHPUnit_Framework_MockObject_MockObject|Bolt_Boltpay_FeatureswitchesController Mocked instance of the class being tested
+     */
+    private $currentMock;
+
+    /**
+     * @var MockObject|Bolt_Boltpay_Model_FeatureSwitch
+     */
+    private $featureSwitchMock;
+
+    /**
+     * Setup test dependencies, called before each test
+     *
+     * @throws Mage_Core_Exception if unable to stub helper
+     */
+    protected function setUp()
+    {
+        $this->currentMock = $this->getTestClassPrototype()
+            ->setMethods(array('sendResponse'))
+            ->getMock();
+        $this->featureSwitchMock = $this->getMockBuilder('Bolt_Boltpay_Model_FeatureSwitch')
+            ->setMethods(array('updateFeatureSwitches'))->getMock();
+        Bolt_Boltpay_TestHelper::stubSingleton('boltpay/featureSwitch', $this->featureSwitchMock);
+    }
+
+    protected function tearDown()
+    {
+        Bolt_Boltpay_TestHelper::restoreSingleton('boltpay/featureSwitch');
+        parent::tearDown();
+    }
+
+    /**
+     * @test
+     * when call updateAction and updateFeatureSwitches call return success response
+     *
+     * @covers ::updateAction
+     */
+    public function updateAction_whenUpdateFeatureSwithesReturnTrue_returnsSuccessResponse()
+    {
+        $this->featureSwitchMock->expects($this->once())->method('updateFeatureSwitches')
+            ->willReturn(true);
+        $this->currentMock->expects($this->once())->method('sendResponse')
+            ->with(
+                200,
+                array('status' => 'success')
+            );
+        $this->currentMock->updateAction();
+    }
+
+    /**
+     * @test
+     * when call updateAction and updateFeatureSwitches call return failure response
+     *
+     * @covers ::updateAction
+     */
+    public function updateAction_whenUpdateFeatureSwithesReturnFalse_returnsFailureResponse()
+    {
+        $this->featureSwitchMock->expects($this->once())->method('updateFeatureSwitches')
+            ->willReturn(false);
+        $this->currentMock->expects($this->once())->method('sendResponse')
+            ->with(
+                422,
+                array('status' => 'failure')
+            );
+        $this->currentMock->updateAction();
+    }
+
+    /**
+     * @test
+     * when call _construct should set requestMustBeSigned to False
+     *
+     * @covers ::_construct
+     */
+    public function _construct_always_shouldSetRequestMustBeSignedToFalse()
+    {
+        TestHelper::setNonPublicProperty($this->currentMock, 'requestMustBeSigned', true);
+        $this->currentMock->_construct();
+        $this->assertFalse(TestHelper::getNonPublicProperty($this->currentMock, 'requestMustBeSigned'));
+    }
+}
