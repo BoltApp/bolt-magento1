c27b7f07b5f9680d629ca84ce56baca4a1bbece1
Aden <adentong@hotmail.com>
2020-03-30 12:43:02 -0400

    [M1P-21] Use our own docker images (#727)
    Use our own docker images

--- tests/scripts/setup_and_test.sh
+++ tests/scripts/setup_and_test.sh
@@ -5,21 +5,8 @@ set -u
 set -x
 
 echo "Installing magento..."
-# Fix the error when trying to fetch jessie backports repository
-echo "deb [check-valid-until=no] http://archive.debian.org/debian jessie-backports main" > /etc/apt/sources.list.d/jessie-backports.list
-sed -i '/deb http:\/\/deb.debian.org\/debian jessie-updates main/d' /etc/apt/sources.list
-# Since Jessie is no longer the most recent stable release for Debian, we have to set Acquire::Check-Valid-Until=false
-apt-get -o Acquire::Check-Valid-Until=false update
-if [ "$1" == "nocov" ]; then
-  apt-get -y install curl php5-curl mysql-client php5-mcrypt
-else
-  apt-get -y install curl php5-curl mysql-client php5-mcrypt php5-xdebug
-fi
-php5enmod mcrypt
-
 curl -O https://files.magerun.net/n98-magerun.phar
 chmod +x n98-magerun.phar
-
 MAGENTO_DIR='./magento'
 php -d memroy_limit=512M n98-magerun.phar install --magentoVersionByName=$MAGENTO_VERSION \
   --installationFolder="${MAGENTO_DIR}" --dbHost=127.0.0.1 --dbUser=root --dbName=circle_test  --dbPass="" \
@@ -28,3 +15,13 @@ php n98-magerun.phar config:set currency/options/base USD --root-dir $MAGENTO_DI
 php n98-magerun.phar config:set currency/options/default USD --root-dir $MAGENTO_DIR
 php n98-magerun.phar config:set currency/options/allow USD --root-dir $MAGENTO_DIR
 cp -r magento/. .
+
+export TEST_ENV=local
+export PHPUNIT_ENVIRONMENT=true
+
+if [ "$1" == "nocov" ]; then
+  php ${PHPUNIT_PHAR} --verbose --stderr --report-useless-tests -c tests/unit/phpunit.xml
+else
+  php ${PHPUNIT_PHAR} --verbose --stderr --report-useless-tests -c tests/unit/phpunit.xml --coverage-clover=./artifacts/coverage.xml
+  bash <(curl -s https://bolt-devops.s3-us-west-2.amazonaws.com/testing/codecov_uploader) -f ./artifacts/coverage.xml -F $TEST_ENV
+fi
