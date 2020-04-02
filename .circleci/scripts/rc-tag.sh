81e87916a1cd8f75669a049f00b82dda1cd2e28a
Aden Dong <aden@bolt.com>
2020-04-02 11:47:03 -0400

    Fix autotagger

--- .circleci/scripts/rc-tag.sh
+++ .circleci/scripts/rc-tag.sh
@@ -4,18 +4,18 @@ set -e
 set -u
 set -x
 
-RCENTRY=$(git for-each-ref --sort=-taggerdate --format="%(refname:short) | %(creatordate)" refs/tags/* | grep "rc " | head -n 1)
-TAG=$(echo $RCENTRY | cut -d"|" -f1)
-TAGDATE=TAG=$(echo $RCENTRY | cut -d"|" -f2)
+PREVRC=$(git for-each-ref --sort=-creatordate --format="%(refname:short) | %(creatordate)" refs/tags/* | grep "0-rc " | head -n 1)
 
-export NEWTAGNAME=$(echo $TAG | awk -F. '{print $1 "." $2+1 ".0-rc"}')
-threeWeeksAgo="21 days ago"
-threeWeekDate=$(date --date "$threeWeeksAgo" +'%s')
+TAGDATE=$(echo $PREVRC | cut -d"|" -f2)
 taggedDate=$(date --date "$TAGDATE" +'%s')
+threeWeekDate=$(date --date "21 days ago" +'%s')
 
 if [[ ${taggedDate} -lt ${threeWeekDate} ]];
 then
-    export TAGTHISWEEK=true
+  export TAGTHISWEEK=true
 else
-    export TAGTHISWEEK=false
+  export TAGTHISWEEK=false
 fi
+
+TAG=$(echo $PREVRC | cut -d"|" -f1)
+export NEWTAGNAME=$(echo $TAG | awk -F. '{print $1 "." $2+1 ".0-rc"}')
