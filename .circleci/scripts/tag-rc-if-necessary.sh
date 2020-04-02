2364d7339134a7b4c380c17bcb1a34f5e94e1ed7
Aden Dong <aden@bolt.com>
2020-04-02 17:42:43 -0400

    .

--- .circleci/scripts/tag-rc-if-necessary.sh
+++ .circleci/scripts/tag-rc-if-necessary.sh
@@ -17,10 +17,10 @@ if [[ ${taggedDate} -lt ${threeWeekDate} ]]; then
   git tag $NEWTAGNAME
   git push origin $NEWTAGNAME
 
-  SLACK_MENTIONS="<@aden>"
+  SLACK_MENTIONS="<@oleksii> <@Pavel>"
   curl -X POST -H "Content-type: application/json" --data "{
     \"attachments\": [{
-      \"text\": \"$NEWTAGNAME $SLACK_MENTIONS\",
+      \"text\": \":white_check_mark: A new RC $NEWTAGNAME has been tagged for Magento 1! $SLACK_MENTIONS\",
       \"color\": \"#58a359\"
     }]
   }" $SLACK_MAGENTO1_WEBHOOK
