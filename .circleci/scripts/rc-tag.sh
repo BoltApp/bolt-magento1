96c9d7b10d9ae1362878dabe6955f2e64c1a631c
Aden Dong <aden@bolt.com>
2020-04-02 12:36:51 -0400

    .

--- .circleci/scripts/rc-tag.sh
+++ .circleci/scripts/rc-tag.sh
@@ -4,7 +4,7 @@ set -e
 set -u
 set -x
 
-PREVRC=$(git for-each-ref --sort=-creatordate --format="%(refname:short) | %(creatordate)" refs/tags/* | grep "0-rc " | head -n 1)
+PREVRC=$(git for-each-ref --sort=-creatordate --format="%(refname:short)|%(creatordate)" refs/tags/* | grep "0-rc " | head -n 1)
 
 TAGDATE=$(echo $PREVRC | cut -d"|" -f2)
 taggedDate=$(date --date "$TAGDATE" +'%s')
@@ -13,12 +13,8 @@ threeWeekDate=$(date --date "21 days ago" +'%s')
 if [[ ${taggedDate} -lt ${threeWeekDate} ]];
 then
   export TAGTHISWEEK=true
+  TAG=$(echo $PREVRC | cut -d"|" -f1)
+  export NEWTAGNAME=$(echo $TAG | awk -F. '{print $1 "." $2+1 ".0-rc"}')
 else
   export TAGTHISWEEK=false
 fi
-
-TAG=$(echo $PREVRC | cut -d"|" -f1)
-export NEWTAGNAME=$(echo $TAG | awk -F. '{print $1 "." $2+1 ".0-rc"}')
-
-echo $TAGTHISWEEK
-echo $NEWTAGNAME
