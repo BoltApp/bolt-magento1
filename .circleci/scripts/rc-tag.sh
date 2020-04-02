1b9a6bf86330e66a5993ab423fda20fb9a494162
Aden Dong <aden@bolt.com>
2020-04-02 13:35:53 -0400

    .

--- .circleci/scripts/rc-tag.sh
+++ .circleci/scripts/rc-tag.sh
@@ -4,18 +4,13 @@ set -e
 set -u
 set -x
 
+echo $PWD
+
 PREVRC=$(git for-each-ref --sort=-creatordate --format="%(refname:short)|%(creatordate)" refs/tags/* | grep "0-rc " | head -n 1)
 
 TAGDATE=$(echo $PREVRC | cut -d"|" -f2)
 taggedDate=$(date --date "$TAGDATE" +'%s')
 threeWeekDate=$(date --date "21 days ago" +'%s')
-
-TAG=$(echo $PREVRC | cut -d"|" -f1)
-echo $PREVRC
-echo $TAGDATE
-echo $taggedDate
-echo $threeWeekDate
-echo $TAG
 
 if [[ ${taggedDate} -lt ${threeWeekDate} ]];
 then
