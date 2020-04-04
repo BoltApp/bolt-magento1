1b4a300b09864ceb1d85c88bbca5bdfba16a2bb0
daisy1754 <d-part@hotmail.co.jp>
2020-04-04 14:34:04 -0700

    tweak

--- tests/scripts/setup_integration.sh
+++ tests/scripts/setup_integration.sh
@@ -19,6 +19,8 @@ mysql -u root -h 127.0.0.1 circle_test < magento-sample-data-1.9.2.4/magento_sam
 php -d memroy_limit=512M n98-magerun.phar install --magentoVersionByName=magento-mirror-1.9.3.6 \
   --installationFolder=/var/www/html --forceUseDb --noDownload --dbHost=127.0.0.1 --dbUser=root --dbName=circle_test  --dbPass="" \
   --installSampleData=yes --useDefaultConfigParams=yes --baseUrl=http://m1-test.integrations.dev.bolt.me
+php -d memroy_limit=512M n98-magerun.phar admin:user:create bolttest dev+m1-integration-admin@bolt.com bolt1234 \
+  --root-dir /var/www/html --no-interaction
 
 cd /var/www/html
 php init-bolt.php $BOLT_SANDBOX_MERCHANT_API_KEY $BOLT_SANDBOX_MERCHANT_SIGNING_SECRET $BOLT_SANDBOX_MERCHANT_PUBLISHABLE_KEY
