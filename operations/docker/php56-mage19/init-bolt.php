c1915d570c3183f5a1e0157cc04298908b44d034
daisy1754 <d-part@hotmail.co.jp>
2020-04-08 09:10:04 -0700

    add_button_everywhere

--- operations/docker/php56-mage19/init-bolt.php
+++ operations/docker/php56-mage19/init-bolt.php
@@ -18,6 +18,7 @@ Mage::getModel('core/config')->saveConfig('currency/options/default', "USD");
 Mage::getModel('core/config')->saveConfig('carriers/flatrate/active', 1);
 Mage::getModel('core/config')->saveConfig('payment/boltpay/active', 1);
 Mage::getModel('core/config')->saveConfig('payment/boltpay/test', 1);
+Mage::getModel('core/config')->saveConfig('payment/boltpay/add_button_everywhere', 1);
 Mage::getModel('core/config')->saveConfig('payment/boltpay/api_key', $api_key);
 Mage::getModel('core/config')->saveConfig('payment/boltpay/signing_key', $signing_key);
 Mage::getModel('core/config')->saveConfig('payment/boltpay/publishable_key_multipage', $publishable_key_multipage);
