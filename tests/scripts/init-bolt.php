87efd410a01dac987628fe06fa2c5dc0023ea28e
Aden Dong <aden@bolt.com>
2020-04-29 12:57:13 -0400

    .

--- tests/scripts/init-bolt.php
+++ tests/scripts/init-bolt.php
@@ -4,14 +4,14 @@ include_once 'app/Mage.php';
 
 Mage::init();
 
-$api_key=Mage::getModel('core/encryption')->encrypt($argv[1]);
-$signing_key=Mage::getModel('core/encryption')->encrypt($argv[2]);
-$publishable_key_multipage=$argv[3];
-$publishable_key_paymentonly=$argv[4];
-$publishable_key_admin=$argv[5];
+$api_key = Mage::getModel('core/encryption')->encrypt($argv[1]);
+$signing_key = Mage::getModel('core/encryption')->encrypt($argv[2]);
+$publishable_key_multipage = $argv[3];
+$publishable_key_paymentonly = $argv[4];
+$publishable_key_admin = $argv[5];
 
 // Save configs
-Mage::getModel('core/config')->saveConfig('general/locale/code','en_US');
+Mage::getModel('core/config')->saveConfig('general/locale/code', 'en_US');
 Mage::getModel('core/config')->saveConfig('currency/options/allow', "USD");
 Mage::getModel('core/config')->saveConfig('currency/options/base', "USD");
 Mage::getModel('core/config')->saveConfig('currency/options/default', "USD");
@@ -26,7 +26,8 @@ Mage::getModel('core/config')->saveConfig('payment/boltpay/publishable_key_onepa
 Mage::getModel('core/config')->saveConfig('payment/boltpay/publishable_key_admin', $publishable_key_admin);
 
 // Create discounts
-function createDiscountRule($couponCode, $action, $additionalData) {
+function createDiscountRule($couponCode, $action, $additionalData)
+{
     $rule = Mage::getModel('salesrule/rule');
     $rule->setName($couponCode)
         ->setDescription($couponCode)
@@ -36,7 +37,7 @@ function createDiscountRule($couponCode, $action, $additionalData) {
         ->setUsesPerCustomer(0)
         ->setUsesPerCoupon(0)
         // for some reason Mage::getModel('customer/group')->getCollection()->getAllIds(); doesn't work
-        ->setCustomerGroupIds(array(0,1,2,4,5))
+        ->setCustomerGroupIds(array(0, 1, 2, 4, 5))
         ->setIsActive(1)
         ->setConditionsSerialized('')
         ->setActionsSerialized('')
@@ -60,17 +61,17 @@ function createDiscountRule($couponCode, $action, $additionalData) {
 }
 
 createDiscountRule("30poff", Mage_SalesRule_Model_Rule::BY_PERCENT_ACTION, array(
-    'discount_amount'         => '30',
+    'discount_amount' => '30',
 ));
 createDiscountRule("1234centsoff", Mage_SalesRule_Model_Rule::BY_FIXED_ACTION, array(
-    'discount_amount'         => '12.34',
+    'discount_amount' => '12.34',
 ));
 createDiscountRule("freeship", Mage_SalesRule_Model_Rule::BY_FIXED_ACTION, array(
-    'simple_free_shipping'         => '1',
+    'simple_free_shipping' => '1',
 ));
 createDiscountRule("expired", Mage_SalesRule_Model_Rule::BY_FIXED_ACTION, array(
-    'from_date'         => '2000-01-01',
-    'to_date'           => '2000-01-02',
+    'from_date' => '2000-01-01',
+    'to_date' => '2000-01-02',
 ));
 
 // Bump inventory of aviator sunglasses
--- tests/scripts/setup_integration.sh
+++ tests/scripts/setup_integration.sh
@@ -13,21 +13,21 @@ sudo rsync -a project/ /var/www/html/
 
 echo "Waiting for DB..."
 while ! sudo mysql -uroot -h localhost -e "SELECT 1" >/dev/null 2>&1; do
-    sleep 1
+  sleep 1
 done
 
 sudo mysql -u root -h localhost -e "CREATE USER 'magento'@'127.0.0.1'"
 sudo mysql -u root -h localhost -e "GRANT ALL PRIVILEGES ON *.* TO 'magento'@'127.0.0.1' WITH GRANT OPTION"
 sudo mysql -u root -h localhost -e "FLUSH PRIVILEGES"
 mysql -u magento -h 127.0.0.1 -e "CREATE DATABASE IF NOT EXISTS magento"
-mysql -u magento -h 127.0.0.1 magento < magento-sample-data-1.9.2.4/magento_sample_data_for_1.9.2.4.sql
+mysql -u magento -h 127.0.0.1 magento <magento-sample-data-1.9.2.4/magento_sample_data_for_1.9.2.4.sql
 php -d memory_limit=512M n98-magerun.phar install --magentoVersionByName=magento-mirror-1.9.3.6 \
-  --installationFolder=/var/www/html --forceUseDb --noDownload --dbHost=127.0.0.1 --dbUser=magento --dbName=magento  --dbPass="" \
+  --installationFolder=/var/www/html --forceUseDb --noDownload --dbHost=127.0.0.1 --dbUser=magento --dbName=magento --dbPass="" \
   --installSampleData=yes --useDefaultConfigParams=yes --baseUrl=http://m1-test.integrations.dev.bolt.me
 php -d memory_limit=512M n98-magerun.phar admin:user:create bolttest dev+m1-integration-admin@bolt.com bolt1234 \
   --root-dir /var/www/html --no-interaction
 
-INC_NUM=$((100*${CIRCLE_BUILD_NUM}))
+INC_NUM=$((100 * ${CIRCLE_BUILD_NUM}))
 mysql -u magento -h 127.0.0.1 -e "SET SQL_MODE='ALLOW_INVALID_DATES'; USE magento; ALTER TABLE sales_flat_quote AUTO_INCREMENT=${INC_NUM};"
 
 cd /var/www/html
