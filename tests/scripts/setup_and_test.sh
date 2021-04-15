eba81d23e7a16db760179089a8009b1d0479188b
Matthew Thomason <matthew@bolt.com>
2021-04-15 16:20:06 -0400

    Remove codecov

--- tests/scripts/setup_and_test.sh
+++ tests/scripts/setup_and_test.sh
@@ -27,5 +27,4 @@ if [ "$1" == "nocov" ]; then
   php ${PHPUNIT_PHAR} --verbose --stderr --report-useless-tests -c tests/unit/phpunit.xml
 else
   php ${PHPUNIT_PHAR} --verbose --stderr --report-useless-tests -c tests/unit/phpunit.xml --coverage-clover=./artifacts/coverage.xml
-  bash <(curl -s https://bolt-devops.s3-us-west-2.amazonaws.com/testing/codecov_uploader) -f ./artifacts/coverage.xml -F $TEST_ENV
 fi
